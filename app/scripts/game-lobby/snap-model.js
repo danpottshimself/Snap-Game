068c1d4ce364f38f399bc675b279308b2d295566
Daniel Potts <Daniel.Potts@tombola.com>
2015-12-10 16:51:30 +0000

    Added images. Got cards showing on screen when clicking play. Shuffling works. Added button for snap button. Next step: Add win functionality.

--- app/scripts/game-lobby/snap-model.js
+++ app/scripts/game-lobby/snap-model.js
@@ -2,19 +2,13 @@
     'use strict';
     angular.module('Tombola.Module.GameLobby')
         .service('SnapModel',
-        function ($state) {
+        function ($state, $timeout, $interval) {
             var me = this,
                 suits = ["Clubs", "Diamonds", "Hearts", "Spades"],
                 cardNumbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"],
                 cards = [];
-            me.init = function () {
-                deck();
-            };
-            me.stateChanger = function () {
-                goToHome();
-            };
-            me.showCards = function(){
-            };
+            me.chosenCard = null;
+
 
             var deck = function () {
                     var suit,
@@ -22,13 +16,15 @@
                     for (suit in suits) {
                         for (rank in cardNumbers) {
                             cards.push({
-                                suit: suit,
-                                value: rank
-                            });
+                                suit: suits[suit],
+                                value: cardNumbers[rank],
+                                image: suits[suit] + cardNumbers[rank]
+                                });
                         }
                     }
                     shuffle(cards);
                 },
+
                 shuffle = function (cards) {
                     var counter = cards.length,
                         temp,
@@ -44,8 +40,26 @@
                     }
                     return cards;
                 },
+
                 goToHome = function () {
                     $state.go('homePage');
                 };
+
+            me.init = function () {
+                deck();
+            };
+
+            me.showCard = function () {
+                me.hideMe = false;
+                var i = 0;
+                    $interval(function () {
+                    me.chosenCard = cards[i];
+                    i++;
+                }, 2000, cards.length);
+            };
+
+            me.stateChanger = function () {
+                goToHome();
+            };
         });
 })();
